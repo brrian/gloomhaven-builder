98e0575427b657fa6d1798d9d4ed97821986c6a2
Brian Wang <brian.wang@thomsonreuters.com>
2018-09-15 20:00:47 -0500

    Extract Scenario related items to <Scenario> component

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,13 +1,12 @@
-import { chain, sample } from 'lodash';
-import React, { Component } from 'react';
+import { sample } from 'lodash';
+import React, { Component, createRef } from 'react';
 import tileData from '../../tileData.json';
-import Tile from '../Tile/Tile';
 import './Map.css';
 import sampleScenario from './sample-scenario.json';
+import Scenario from '../Scenario/Scenario';
 
 class Map extends Component {
   state = {
-    availableHooks: [],
     mapX: 0,
     mapY: 0,
     scale: .35,
@@ -15,21 +14,22 @@ class Map extends Component {
     selectedTile: '',
     selectedTileCoords: '0px, 0px',
     selectedTileRotation: 0,
-    tiles: [],
   };
+
+  constructor(props) {
+    super(props);
+
+    this.scenario = createRef();
+  }
 
   componentDidMount() {
     document.addEventListener('keydown', this.handleKeyDown);
-
-    if (this.state.scenario.connections.length) {
-      this.placeExistingTiles();
-    }
   }
 
   handleMapClick = (event) => {
     const { scale, selectedTile, selectedTileRotation } = this.state;
 
-    if (event.target.dataset.tile) {
+    if (false && event.target.dataset.tile) {
       const data = tileData[event.target.dataset.tile];
       const tile = this.state.tiles.find(({ name }) => name === event.target.dataset.tile);
 
@@ -59,8 +59,10 @@ class Map extends Component {
     }
 
     if (selectedTile) {
-      this.placeTile(selectedTile, event.clientX, event.clientY, selectedTileRotation)
-        .then(this.connectPlacedTileIfPossible.bind(this, selectedTile));
+      this.scenario.current.placeTile(selectedTile, event.clientX, event.clientY, selectedTileRotation)
+        .then(() => this.scenario.current.connectPlacedTileIfPossible(selectedTile));
+
+      this.setState({ selectedTile: null, selectedTileRotation: 0 });
     }
   }
 
@@ -124,119 +126,15 @@ class Map extends Component {
     }
   }
 
-  placeExistingTiles() {
-    const { scenario: { connections } } = this.state;
-
-    const screenXMidpoint = window.innerWidth /2;
-    const screenYMidpoint = window.innerHeight /2;
-
-    connections.reduce((promiseChain, connection) => {
-      const { anchor, hook } = connection;
-      const [ anchorTile ] = anchor.split('-');
-      const [ hookTile ] = hook.split('-');
-
-      const anchorData = this.state.scenario.tiles.find(tile => tile.name === anchorTile);
-      const hookData = this.state.scenario.tiles.find(tile => tile.name === hookTile);
-
-      return promiseChain.then(chainResults => {
-        const connectionPromise = Promise.all([
-          this.placeTile(anchorTile, screenXMidpoint, screenYMidpoint, anchorData.rotation),
-          this.placeTile(hookTile, screenXMidpoint, screenYMidpoint, hookData.rotation),
-        ]).then(this.connectTile.bind(this, connection));
-
-        return connectionPromise.then(result => [ ...chainResults, result ])
-      }
-      );
-    }, Promise.resolve([]));
-  }
-
-  placeTile(name, x, y, rotation) {
-    return new Promise((resolve, reject) => {
-      const { scale, tiles, mapX, mapY } = this.state;
-      const { anchors, width, height } = tileData[name];
-
-      if (tiles.find(tile => tile.name === name)) return resolve();
-
-      const tile = {
-        name,
-        x: (x / scale) - (width / 2) - (mapX / scale),
-        y: (y / scale) - (height / 2) - (mapY / scale),
-        rotation: rotation % 360,
-      }
-
-      const hooks = anchors.map((pos, index) => `${name}-${index}`);
-
-      this.setState(prevState => ({
-        selectedTile: null,
-        selectedTileRotation: 0,
-        tiles: [ ...prevState.tiles, tile ],
-        availableHooks: [
-          ...prevState.availableHooks,
-          ...hooks,
-        ]
-      }), resolve);
-    });
-  }
-
-  connectTile(connection) {
-    return new Promise((resolve, reject) => {
-      const [ hookTile ] = connection.hook.split('-');
-
-      const anchor = document.getElementById(connection.anchor);
-      const hook = document.getElementById(connection.hook);
-
-      const { x: anchorX, y: anchorY } = anchor.getBoundingClientRect();
-      const { x: hookX, y: hookY } = hook.getBoundingClientRect();
-
-      this.setState(({ scale, tiles: prevTiles }) => {
-        const tiles = [ ...prevTiles ];
-        const updatedHook = tiles.find(({ name }) => name === hookTile);
-        updatedHook.x += (anchorX - hookX) / scale;
-        updatedHook.y += (anchorY - hookY) / scale;
-        return { tiles }
-      }, resolve);
-    });
-  }
-
-  connectPlacedTileIfPossible(tile) {
-    const { availableHooks } = this.state;
-
-    const anchors = availableHooks
-      .filter(id => id.indexOf(tile) === -1)
-      .map(id => ({ id, bounds: document.getElementById(id).getBoundingClientRect() }));
-
-    const hooks = availableHooks
-      .filter(id => id.indexOf(tile) !== -1)
-      .map(id => ({ id, bounds: document.getElementById(id).getBoundingClientRect() }));
-
-    const match = chain(anchors)
-      .map(anchor => hooks.map(hook => {
-        const leftBounds = anchor.bounds.right - hook.bounds.left;
-        const rightBounds = anchor.bounds.left - hook.bounds.right;
-        const topBounds = anchor.bounds.top - hook.bounds.bottom;
-        const bottomBounds = anchor.bounds.bottom - hook.bounds.top;
-
-        const doesOverlap = leftBounds > 0 && rightBounds < 0 && topBounds < 0 && bottomBounds > 0;
-        const closeness = Math.abs(leftBounds + rightBounds + topBounds + bottomBounds);
-
-        return doesOverlap ? { anchor: anchor.id, hook: hook.id, closeness } : false;
-      }))
-      .flatten().compact().sortBy('closeness').first().value();
-
-    if (match) {
-      this.connectTile(match);
-    }
-  }
-
   render() {
     const {
       mapX,
       mapY,
       scale,
+      scenario,
       selectedTile,
       selectedTileCoords,
       selectedTileRotation,
-      tiles,
     } = this.state;
 
     return (
@@ -268,7 +166,7 @@ class Map extends Component {
         <div className="map" style={{
           transform: `translate(${mapX}px, ${mapY}px) scale(${scale})`,
         }}>
-          {tiles.map(tile => <Tile key={tile.name} {...tile} />)}
+          <Scenario ref={this.scenario} {...scenario} map={{ scale, x: mapX, y: mapY }} />
         </div>
       </div>
     )
