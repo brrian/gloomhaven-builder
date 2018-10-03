76149a2b6eacf40467a7ceec0aa65d950651b1e2
Brian Wang <brian.wang@thomsonreuters.com>
2018-10-03 12:35:18 -0500

    Clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import Map from './Components/Map/Map';
 import { IScenario } from './Components/Map/types';
 import sampleScenario from './sample-scenario.json';
